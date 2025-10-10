17050c162a239a6d432b64606e9a4ea469633304
Kerwin2712 <kerwin27120201@gmail.com>
2025-10-10 17:53:21 -0400

    debug: reporde de actividad diaria mejorado

--- utils/notion_utils.py
+++ utils/notion_utils.py
@@ -1,6 +1,6 @@
 import os
 from notion_client import Client
-from datetime import datetime
+from datetime import datetime, timezone
 import config
 
 # Inicializa el cliente de Notion
@@ -8,10 +8,14 @@ notion = Client(auth=os.getenv("NOTION_TOKEN"))
 
 def add_activity_log(id_member: str, nombre: str, entrada: bool, canal: str, tiempo_coneccion: int = None):
     #print(f"[DEBUG] add_activity_log llamada con: id_member={id_member}, nombre={nombre}, entrada={entrada}, canal={canal}, tiempo_coneccion={tiempo_coneccion}")
+    
+    # Usar `now(timezone.utc)` para asegurar que la fecha y hora estén en UTC.
+    now_utc = datetime.now(timezone.utc).isoformat()
+
     properties = {
         "id_member": {"title": [{"text": {"content": id_member}}]},
         "nombre": {"rich_text": [{"text": {"content": nombre}}]},
-        "fecha_hora": {"date": {"start": datetime.now().isoformat()}},
+        "fecha_hora": {"date": {"start": now_utc}},
         "entrada": {"checkbox": entrada},
         "canal": {"rich_text": [{"text": {"content": canal}}]},
     }
@@ -55,7 +59,36 @@ def find_last_connection(id_member: str, canal: str):
         print(f"Error al buscar el último registro de conexión en Notion: {e}")
         return None
 
-
+def get_exit_logs_for_today():
+    """
+    Recupera todos los registros de SALIDA de actividad del día actual desde Notion.
+    """
+    try:
+        today_utc = datetime.now(timezone.utc).date().isoformat()
+        
+        response = notion.databases.query(
+            database_id=config.NOTION_DATABASE_ACTIVIDAD_ID,
+            filter={
+                "and": [
+                    {
+                        "property": "fecha_hora",
+                        "date": {
+                            "on_or_after": today_utc
+                        }
+                    },
+                    {
+                        "property": "entrada",
+                        "checkbox": {
+                            "equals": False
+                        }
+                    }
+                ]
+            }
+        )
+        return response.get("results", [])
+    except Exception as e:
+        print(f"Error al obtener los registros de salida de Notion: {e}")
+        return []
 
 def get_activity_logs_for_today():
     """
